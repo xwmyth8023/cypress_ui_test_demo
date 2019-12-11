8979bd72116a03ad1587dbc2f8beb8eaae3086cd
Sandy <sandy@sandy.intranet.theknot.com>
2019-12-11 15:40:23 +0800

    add best of baby

---
+++ cypress/integration/testBestBabyList.js
@@ -0,0 +1,23 @@
+/// <reference types="Cypress" />
+
+const data = ["title","description","keyWords","ogTitle","ogDescription","ogImage","ogUrl","ogType"]
+
+describe('SEO Check', ()=>{
+   
+  let jsonData
+
+  before(()=>{
+    cy.fixture('bestBabyList.json').then((json) => {
+      jsonData = json
+    })
+    cy.viewport('macbook-15')
+    cy.visit('/best-of-baby')
+    
+  })
+
+  data.forEach(item => {
+    it(`check ${item}`,() => {
+      cy.checkSEO(jsonData,item)
+    })
+  })
+})
